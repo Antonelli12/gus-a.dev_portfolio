4fb3103eec6aa0345a171a78fde7f0dffccbc1c5
Gus A. <antonellir12@gmail.com>
2026-04-24 22:29:57 +0100

    feat: remove deprecated sections and components from the portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { AboutSection } from "@/components/about/about-section";
-import { ContactSection } from "@/components/contact/contact-section";
 import { HeroSection } from "@/components/home/hero-section";
 import { SiteHeader } from "@/components/layout/site-header";
 
